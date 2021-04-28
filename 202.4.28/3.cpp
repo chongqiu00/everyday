56dd84c4cb2ba73c71a7b245ace0b27073bca6bd
chongqiu00 <1685546690@qq.com>
2021-04-28 21:23:40 +0800

    2021/4/28

---
+++ 202.4.28/3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+
+    int& p = a;
+    
+
+    std::cout << "p=" << p << std::endl;/*5*/
+    std::cout << "a=" << a << std::endl;/*5*/
+
+    p = b;//p作为a改变了a的值。
+
+    std::cout << "p=" << p << std::endl;/*5*/
+    std::cout << "a=" << a << std::endl;/*5*/
+    std::cout << "b=" << b << std::endl;/*5*/
+
+    return 0;
+}
